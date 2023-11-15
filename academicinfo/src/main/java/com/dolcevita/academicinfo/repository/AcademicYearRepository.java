927821e631ec93a73d7959b559035b00c6400863
Ike28 <mihaijrcorocaescu@gmail.com>
2023-11-15 12:17:03 +0200

    Remove unused AcademicYearRepository method

--- academicinfo/src/main/java/com/dolcevita/academicinfo/repository/AcademicYearRepository.java
+++ academicinfo/src/main/java/com/dolcevita/academicinfo/repository/AcademicYearRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.sql.Timestamp;
 
 public interface AcademicYearRepository extends JpaRepository<AcademicYear, Integer> {
-    AcademicYear findByFirstSemesterStartTsBeforeAndSecondSemesterEndTsAfter(final Timestamp timestamp);
 }
