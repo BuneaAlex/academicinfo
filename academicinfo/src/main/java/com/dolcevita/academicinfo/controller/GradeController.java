792cf729d223dec5646afff21211b8d0c281c8bd
razvanclaudiu <tomutarazvan21@gmail.com>
2023-11-15 15:19:27 +0200

    Add Grade Model, Repo, Service and Controller with GET & POST

---
+++ academicinfo/src/main/java/com/dolcevita/academicinfo/controller/GradeController.java
@@ -0,0 +1,29 @@
+package com.dolcevita.academicinfo.controller;
+
+import com.dolcevita.academicinfo.dto.GradeDto;
+import com.dolcevita.academicinfo.service.GradeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class GradeController {
+
+    private final GradeService gradeService;
+
+    @PostMapping("/admin/grades")
+    public GradeDto createGrade(@RequestBody GradeDto gradeDto) {
+        return gradeService.createGrade(gradeDto);
+    }
+
+    @GetMapping("/grades")
+    public Set<GradeDto> getGradesBySubjectUuid(@RequestParam String subjectUuid) {
+        return gradeService.getGradesBySubject(subjectUuid);
+    }
+
+
+}
