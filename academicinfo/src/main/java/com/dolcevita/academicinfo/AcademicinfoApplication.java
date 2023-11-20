e2ee9b0bfaedd5c95156173953db619c3adcd466
Ike28 <mihaijrcorocaescu@gmail.com>
2023-11-20 11:20:05 +0200

    Add Slf4J logging

--- academicinfo/src/main/java/com/dolcevita/academicinfo/AcademicinfoApplication.java
+++ academicinfo/src/main/java/com/dolcevita/academicinfo/AcademicinfoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 public class AcademicinfoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AcademicinfoApplication.class, args);
     }
