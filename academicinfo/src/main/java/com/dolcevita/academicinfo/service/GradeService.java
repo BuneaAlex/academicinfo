f57d12e7a61287ad10415c5c53a1ab5667bdf083
razvanclaudiu <tomutarazvan21@gmail.com>
2023-11-23 14:55:40 +0200

    Fixed Grade GET

--- academicinfo/src/main/java/com/dolcevita/academicinfo/service/GradeService.java
+++ academicinfo/src/main/java/com/dolcevita/academicinfo/service/GradeService.java
@@ -4,6 +4,8 @@ import com.dolcevita.academicinfo.dto.GradeDto;
 import com.dolcevita.academicinfo.exceptions.ResourceNotFoundException;
 import com.dolcevita.academicinfo.model.Grade;
 import com.dolcevita.academicinfo.repository.GradeRepository;
+import com.dolcevita.academicinfo.repository.SubjectRepository;
+import com.dolcevita.academicinfo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Service;
@@ -17,33 +19,36 @@ public class GradeService {
 
     private final GradeRepository gradeRepository;
 
+    private final UserRepository userRepository;
+
+    private final SubjectRepository subjectRepository;
+
     public GradeDto createGrade(final GradeDto newGrade) {
+        val student = userRepository.findByEmail(newGrade.studentEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found"));
+        val subject = subjectRepository.findByUuid(newGrade.subjectUuid());
         val grade = Grade.builder()
                 .grade(newGrade.grade())
-                .student(newGrade.student())
-                .subject(newGrade.subject())
+                .student(student)
+                .subject(subject)
                 .build();
         val created = gradeRepository.save(grade);
         return handleGrade(created);
     }
 
     public Set<GradeDto> getGradesBySubject(final String subjectUuid) {
-        try {
-            return gradeRepository.findAll()
-                    .stream()
-                    .filter(grade -> subjectUuid.equals(grade.getSubject().getUuid()))
-                    .map(this::handleGrade)
-                    .collect(Collectors.toSet());
-        } catch (ResourceNotFoundException ignored) {
-        }
-        return null;
+        val subject = subjectRepository.findByUuid(subjectUuid);
+        return gradeRepository.findAllBySubject(subject)
+                .stream()
+                .map(this::handleGrade)
+                .collect(Collectors.toSet());
     }
 
     private GradeDto handleGrade(final Grade grade) {
         return new GradeDto(grade.getUuid(),
                 grade.getGrade(),
-                grade.getStudent(),
-                grade.getSubject(),
+                grade.getStudent().getEmail(),
+                grade.getSubject().getUuid(),
                 grade.getCreatedAt(),
                 grade.getUpdatedAt()
         );
